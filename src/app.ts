56670698ad26a0c3b2191771ea119d756462c982
workonitapp <office@workonit.co.il>
2022-08-03 13:58:25 +0300

    first commit

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,22 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import axios from 'axios';
-import getMostLikelyNationality from './getMostLikelyNationality';
-import getMostLikelyGender from './getMostLikelyGender';
 
 const app: Application = express();
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'http://localhost:3001',
   optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
 
-app.get('/whoami', async (req: Request, res: Response, next: NextFunction) => {
-  const name: string = req.query.name as string;
+app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({ message: 'Hi!' });
+});
 
-  if (!/^[A-Za-z]*$/.test(name))
-    return res.status(400).json({ err: 'Name must be in English' });
-
-  if (!name)
-    return res.status(400).json({ err: 'Please provide a name to guess.' });
-
-  try {
-    const mostLikelyNationality = await getMostLikelyNationality(name);
-    const mostLikelyGender = await getMostLikelyGender(name);
-
-    res.status(200).json({
-      nationality: mostLikelyNationality,
-      gender: mostLikelyGender,
-    });
-  } catch (err: any) {
-    console.log(err);
-    res.status(500).json({ err: err.message });
-  }
+app.post('/', async (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({});
 });
 
 app.listen(5000, () => console.log('Server running on port 5000'));
